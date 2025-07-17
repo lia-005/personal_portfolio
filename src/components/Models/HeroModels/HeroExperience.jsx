2030add838905262d56fa6cfc71d6e9c34e65b63
lia-005 <dipannitachowdhury81@gmail.com>
2025-07-17 21:29:46 +0530

    deleted fallbackImage

--- src/components/Models/HeroModels/HeroExperience.jsx
+++ src/components/Models/HeroModels/HeroExperience.jsx
@@ -1,47 +1,20 @@
-import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import HeroLights from "../HeroModels/Herolight";
 import Partical from "./Partical";
 import { useMediaQuery } from "react-responsive";
 import { OrbitControls } from "@react-three/drei";
 import { Model } from "./Animegirl";
-import ErrorBoundary from "../HeroModels/ErrorBoundary"; 
+
 
 const HeroExperience = () => {
   const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
 
-  const fallbackImage = (
-    <div style={{ width: "100%", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
-      <style>
-       {`.profile {width: "80%";
-       maxWidth: "400px"; borderRadius: "50%";
-        transform: "translatey(0px)";
-        animation: "float 6s ease-in-out infinite";}
-        @keyframes float {
-          0 % {
 
-            transform: translatey(0px);
-          }
-  50% {
-
-          transform: translatey(-20px);
-  }
-        100% {
-
-          transform: translatey(0px);
-       }}
-  `}
-      </style>
-
-      <img src="/assets/images/profile.png" alt="Character fallback" className="profile" />
-
-    </div>
-  );
 
   return (
-    <ErrorBoundary fallback={isMobile ? fallbackImage : null}>
-      <Suspense fallback={isMobile ? fallbackImage : null}>
+
+
         <Canvas camera={{ position: isMobile ? [0, 0, 20] : [0, 0, 15], fov: 45 }}>
           <OrbitControls
             enablePan={false}
@@ -62,8 +35,7 @@ const HeroExperience = () => {
           <Model />
 
         </Canvas>
-      </Suspense>
-    </ErrorBoundary>
+    
   );
 };
 
